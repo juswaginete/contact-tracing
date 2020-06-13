aa3f359f5c188e1b5d46875cc16f9680313500ae
Xavier Cagmat <xavierallyster@gmail.com>
2020-06-13 20:32:59 +0800

    frontend setup

--- contact_tracing/settings/settings.py
+++ contact_tracing/settings/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'contact_tracing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,6 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets/'),
+]
 
 # LOCAL SETTINGS
 f = os.path.join(BASE_DIR, "contact_tracing", "settings", "local.py")
