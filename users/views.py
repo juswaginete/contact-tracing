752ce3c9eea534f891c8ce356594c4c1b12b0435
James Joshua Ginete <james.ginete10@gmail.com>
2020-06-13 19:44:53 +0800

    Add api views for login and logout endpoints

--- users/views.py
+++ users/views.py
@@ -1,8 +1,23 @@
-from rest_framework import status
+from http import HTTPStatus
+
+from django.contrib.auth import login as django_login, logout as django_logout
+from django.utils import timezone
+
+from rest_framework import (
+    authentication,
+    exceptions,
+    parsers,
+    renderers,
+    status,
+    viewsets,
+    generics,
+    mixins
+)
+from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from users.serializers import ProfileSerializer
+from users.serializers import ProfileSerializer, AuthCustomTokenSerializer
 
 
 class UserProfileView(APIView):
@@ -19,3 +34,49 @@ class UserProfileView(APIView):
             return Response(serializer.process_create_user(request))
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_current_datetime():
+    return timezone.now().astimezone(timezone.get_default_timezone())
+
+
+class ObtainAuthToken(APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = AuthCustomTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data.get('user')
+
+        django_login(request, user)
+        token, created = Token.objects.get_or_create(user=user)
+
+        if not created:
+            user = token.user
+            token.delete()
+            token = Token.objects.create(user=user)
+            token.created = get_current_datetime()
+            token.save()
+
+        return Response({
+            'id': user.id,
+            'token': token.key,
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        }, status=200)
+
+
+class LogoutView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        try:
+            token = Token.objects.get(key=request.user.auth_token)
+        except Token.DoesNotExist:
+            raise exceptions.AuthenticationFailed('Invalid token')
+
+        if not token.user.is_active:
+            raise exceptions.AuthenticationFailed('User inactive or deleted')
+
+        token.delete()
+        return Response(HTTPStatus.OK)
