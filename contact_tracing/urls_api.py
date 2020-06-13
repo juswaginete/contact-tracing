70249b0d2a1272520085d1b4d2129c96135fd5fe
James Joshua Ginete <james.ginete10@gmail.com>
2020-06-13 19:43:17 +0800

    Add url configurations for login and logout endpoints

--- contact_tracing/urls_api.py
+++ contact_tracing/urls_api.py
@@ -3,4 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('signup/', include('users.urls_signup')),
+    path('login/', include('users.urls_login')),
+    path('logout/', include('users.urls_logout')),
 ]
