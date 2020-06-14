3a37d8e4e89c2d8a54697b6eca626f4770192696
James Joshua Ginete <james.ginete10@gmail.com>
2020-06-14 19:41:31 +0800

    Add user_type field

--- users/models.py
+++ users/models.py
@@ -9,11 +9,16 @@ optional = {
 
 
 class Profile(models.Model):
-
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Others')
+    )
+
+    USER_TYPES = (
+        (0, 'Staff'),
+        (1, 'Admin'),
+        (2, 'Church Member')
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
@@ -25,6 +30,7 @@ class Profile(models.Model):
     country = models.CharField(max_length=255, **optional)
     postal_code = models.PositiveIntegerField(**optional)
     phone_number = models.CharField(max_length=30, unique=True, **optional)
+    user_type = models.IntegerField(choices=USER_TYPES, default=0)
 
     def __str__(self):
         return "{} {}".format(self.user.first_name, self.user.last_name)
