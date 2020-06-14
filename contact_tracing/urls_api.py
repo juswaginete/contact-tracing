d94fa613ed0d56e9ff4af515a7eeca6b3784a90e
James Joshua Ginete <james.ginete10@gmail.com>
2020-06-14 21:05:42 +0800

    Add url configuration in getting user profile list and user profile details

--- contact_tracing/urls_api.py
+++ contact_tracing/urls_api.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 from django.urls import include, path
 
+from rest_framework.routers import SimpleRouter
+
+from users.views import UserProfileViewSet
+
+profile_router = SimpleRouter()
+profile_router.register(r'', UserProfileViewSet, basename='profile-list')
+
 urlpatterns = [
     path('signup/', include('users.urls_signup')),
     path('login/', include('users.urls_login')),
     path('logout/', include('users.urls_logout')),
+    path('profiles/', include(profile_router.urls)),
 ]
